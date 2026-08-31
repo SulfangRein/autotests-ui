936833126c666d4fb28a17434868f58b640d694f
SulfangRein <anton.a.chulkov@gmail.com>
2026-08-31 23:44:22 +0700

    маркировки pytest

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,10 +1,10 @@
+from playwright.sync_api import sync_playwright, expect
 import pytest
-from playwright.sync_api import sync_playwright
 
 
-@pytest.mark.registration
+@pytest.mark.courses
 @pytest.mark.regression
-def test_successful_registration():
+def test_empty_courses_list():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
         context = browser.new_context()
@@ -31,6 +31,19 @@ def test_successful_registration():
         context = browser.new_context(storage_state='browser-state.json')
         page = context.new_page()
 
-        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard")
+        page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
 
-        page.wait_for_timeout(5000)
+        courses_title = page.get_by_test_id('courses-list-toolbar-title-text')
+        expect(courses_title).to_be_visible()
+        expect(courses_title).to_have_text('Courses')
+
+        empty_courses_icon = page.get_by_test_id('courses-list-empty-view-icon')
+        expect(empty_courses_icon).to_be_visible()
+
+        empty_title_text = page.get_by_test_id('courses-list-empty-view-title-text')
+        expect(empty_title_text).to_be_visible()
+        expect(empty_title_text).to_have_text('There is no results')
+
+        empty_description_text = page.get_by_test_id('courses-list-empty-view-description-text')
+        expect(empty_description_text).to_be_visible()
+        expect(empty_description_text).to_have_text('Results from the load test pipeline will be displayed here')
