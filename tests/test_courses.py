b23184650011a3b4c025e02b39d7e6b065cf252b
SulfangRein <anton.a.chulkov@gmail.com>
2026-09-01 22:13:47 +0700

    фикстуры в pytest

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -4,7 +4,7 @@ import pytest
 
 @pytest.mark.courses
 @pytest.mark.regression
-def test_empty_courses_list():
+def test_empty_courses_list_old():
     with sync_playwright() as playwright:
         browser = playwright.chromium.launch(headless=False)
         context = browser.new_context()
@@ -45,5 +45,26 @@ def test_empty_courses_list():
         expect(empty_title_text).to_have_text('There is no results')
 
         empty_description_text = page.get_by_test_id('courses-list-empty-view-description-text')
+        expect(empty_description_text).to_be_visible()
+        expect(empty_description_text).to_have_text('Results from the load test pipeline will be displayed here')
+
+
+@pytest.mark.courses
+@pytest.mark.regression
+def test_empty_courses_list(chromium_page_with_state):
+        chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+
+        courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
+        expect(courses_title).to_be_visible()
+        expect(courses_title).to_have_text('Courses')
+
+        empty_courses_icon = chromium_page_with_state.get_by_test_id('courses-list-empty-view-icon')
+        expect(empty_courses_icon).to_be_visible()
+
+        empty_title_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
+        expect(empty_title_text).to_be_visible()
+        expect(empty_title_text).to_have_text('There is no results')
+
+        empty_description_text = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
         expect(empty_description_text).to_be_visible()
         expect(empty_description_text).to_have_text('Results from the load test pipeline will be displayed here')
