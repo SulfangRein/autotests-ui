45366ab580cbba94061e2400a9d6076c2e1ecfbb
SulfangRein <anton.a.chulkov@gmail.com>
2026-09-01 22:01:04 +0700

    фикстуры в pytest

---
+++ tests/conftest.py
@@ -0,0 +1,17 @@
+import pytest
+from playwright.sync_api import Page, Playwright
+
+
+# @pytest.fixture
+# def chromium_page() -> Page:
+#     with sync_playwright() as playwright:
+#         browser = playwright.chromium.launch(headless=False)
+#         yield browser.new_page()
+#         browser.close()
+
+
+@pytest.fixture
+def chromium_page(playwright:Playwright) -> Page:
+    browser = playwright.chromium.launch(headless=False)
+    yield browser.new_page()
+    browser.close()
