b3cfbdd96c153d6dd3d7f43e475d7e91bede6bc5
SulfangRein <anton.a.chulkov@gmail.com>
2026-09-10 17:41:00 +0700

    Знакомство с PageComponent

--- tests/test_registration.py
+++ tests/test_registration.py
@@ -7,6 +7,7 @@ from pages.registration_page import RegistrationPage
 @pytest.mark.regression
 def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
     registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-    registration_page.fill_registration_form("user.name@gmail.com", "username", "password")
+    registration_page.registration_form.fill("user.name@gmail.com", "username", "password")
+    registration_page.registration_form.check_visible("user.name@gmail.com", "username", "password")
     registration_page.click_registration_button()
-    dashboard_page.check_visible_dashboard_title()
+    dashboard_page.dashboard_toolbar_view.check_visible()
